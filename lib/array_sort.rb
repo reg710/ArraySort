8548d5cf777025ef5ad5603aee562c33852b5181
rcon <reg710@gmail.com>
2021-04-01 15:19:10 -0400

    Added tests to spec file and function to class file

--- lib/array_sort.rb
+++ lib/array_sort.rb
@@ -1,8 +1,19 @@
 class Sorter
     
-    def arrange(input)
-        input = [1, 2]
-        return input
+    def arrange(chaos)  
+        looping = true
+        while looping
+            looping = false
+            (0..(chaos.length - 2)).each do |x|
+                if chaos[x] > chaos[x+1]
+                    looping = true
+                    temp = chaos[x]
+                    chaos[x] = chaos[x + 1]
+                    chaos[x + 1] = temp
+                end
+            end
+        end
+        return chaos
     end
 
 end
--- spec/array_sort_spec.rb
+++ spec/array_sort_spec.rb
@@ -5,11 +5,47 @@ describe Sorter do
         expect(true).to eql(true)
     end
 
-    it "returns an array" do
+    it "returns an already sorted array unchanged" do
+        sorter = Sorter.new
+        result = sorter.arrange([1, 2])
+        expect(result).to eql([1, 2])
+    end
+
+    it "sorts an array of 2" do
         sorter = Sorter.new
         result = sorter.arrange([2, 1])
         expect(result).to eql([1, 2])
     end
 
+    it "sorts an array of 3" do
+        sorter = Sorter.new
+        result = sorter.arrange([2, 1, 3])
+        expect(result).to eql([1, 2, 3])
+    end
+
+    it "sorts an array with negative numbers" do
+        sorter = Sorter.new
+        result = sorter.arrange([2, 1, -3])
+        expect(result).to eql([-3, 1, 2])
+    end
+
+    it "sorts an array with decimals" do
+        sorter = Sorter.new
+        result = sorter.arrange([1.7, 1.5, 3])
+        expect(result).to eql([1.5, 1.7, 3])
+    end
+
+    it "sorts an array with fractions" do
+        sorter = Sorter.new
+        result = sorter.arrange([10/4, 1, 100])
+        expect(result).to eql([1, 10/4, 100])
+    end
+
+    it "sorts an array of all the same numbers" do
+        sorter = Sorter.new
+        result = sorter.arrange([100, 100, 100, 100])
+        expect(result).to eql([100, 100, 100, 100])
+    end
+    
 
 end
